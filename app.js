d9fbaa16df88317a0827a4250000d1c67ae49ad0
Malinda Kapuruge <malinda.kapuruge@versent.com.au>
2021-10-01 12:24:44 +1000

    add linter

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const add = require('./lib/util');
 
-//Define request response in root URL (/)
 app.get('/', function (req, res) {
   const a = Number(req.query.a)
   const b = Number(req.query.b)
+  console.log("got request for "+a+' + '+ b )
   res.send(a+' + '+ b +' = ' + add(a,b))
 })
 
 //Launch listening server on port 8080
 app.listen(8080, function () {
-  console.log('app listening on port 8080!')
+  console.log('port 8080!')
 })
